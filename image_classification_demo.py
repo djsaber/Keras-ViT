a7d39a4e5556d317f0d3c7af9506ed482302f4a9
djsaber <60215276+djsaber@users.noreply.github.com>
2023-05-10 18:26:36 +0800

    Update image_classification_demo.py

--- image_classification_demo.py
+++ image_classification_demo.py
@@ -9,9 +9,7 @@ vit_model = vit.ViT_B16(weights="imagenet21k+imagenet2012")
 img = utils.read_img("test.jpg", resize=vit_model.image_size)
 img = img.reshape((1,*vit_model.image_size,3))
 
-# predict
+# print class of the picture
 y = vit_model.predict(img)
 classes = utils.get_imagenet2012_classes()
-
-# print class of the picture
 print(classes[y[0].argmax()])
